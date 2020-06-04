1fcd16b34ffed2f8752a3db1e5da3ed9645ce514
Dima <54286444+dfg007star@users.noreply.github.com>
2020-06-04 23:59:48 +0300

    ~

---
+++ The Vowel Code.js
@@ -0,0 +1,43 @@
+const letters = ['a', 'e', 'i', 'o', 'u'];
+function encode(string) {
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    for (let y = 0; y < letters.length; y++) {
+      if (string[i] === letters[y]) {
+        result += 1 + letters.indexOf(letters[y]);
+        i++;
+        if (i === string.length) {
+          return result;
+        }
+      }
+    }
+    result += string[i];
+  }
+  return result;
+}
+
+function decode(string) {
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    for (let y = 0; y < letters.length; y++) {
+      if (
+        string[i] === '1' ||
+        string[i] === '2' ||
+        string[i] === '3' ||
+        string[i] === '4' ||
+        string[i] === '5'
+      ) {
+        console.log('yes');
+        result += letters[string[i] - 1];
+        i++;
+        if (i === string.length) {
+          return result;
+        }
+      }
+    }
+    result += string[i];
+  }
+  return result;
+}
+
+console.log(encode('l24owr5f2jkjlt4hzwdqqvky') === 'l244wr5f2jkjlt4hzwdqqvky');
